99e99af08cf808f4ae9896df1c140eefd2469ee6
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-08 19:52:10 -0500

    checked box

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double x = 0;
+  double textSize = 10;
+  bool? isChecked = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Style Editor"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer",
+                  style: TextStyle(fontSize: textSize),
+                ),
+                Divider(),
+                Text("Tamaño del texto Slide"),
+                Slider(
+                  max: 30,
+                  min: 5,
+                  value: textSize,
+                  activeColor: Colors.red,
+                  inactiveColor: Colors.green,
+                  thumbColor: Colors.yellow,
+                  onChanged: (double mandarina) {
+                    textSize = mandarina;
+                    print(mandarina);
+                    setState(() {});
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text("Is checked"),
+                  value: isChecked,
+                  onChanged: (juanito) {
+                    isChecked = juanito;
+                    setState(() {});
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
