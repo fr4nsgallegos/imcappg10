d36ed7ee99d97c82a39311b46d118054d6279aaa
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-08 21:25:22 -0500

    texto con imagen

--- lib/pages/rich_text_page.dart
+++ lib/pages/rich_text_page.dart
@@ -5,9 +5,12 @@ class RichTextPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        // backgroundColor: Colors.black,
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              //INTRO RICH TEXT
               RichText(
                 text: TextSpan(
                   text: "Texto básico ",
@@ -17,15 +20,65 @@ class RichTextPage extends StatelessWidget {
                   ),
                   children: [
                     TextSpan(
-                      text: "Richtext",
+                      text: "Richtext ",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.green,
+                        fontSize: 45,
+                        shadows: [
+                          Shadow(
+                            color: Colors.red,
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 10.0,
+                          ),
+                        ],
                       ),
+                    ),
+                    TextSpan(
+                      text: "otro texto",
+                      style: TextStyle(color: Colors.orange, fontSize: 25),
                     ),
                   ],
                 ),
               ),
+
+              //SIMULANDO SUBINDICES
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Colors.cyan, fontSize: 50),
+                  children: [
+                    TextSpan(text: "H"),
+                    TextSpan(
+                      text: "2",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.red,
+                      ),
+                    ),
+                    TextSpan(text: "O"),
+                  ],
+                ),
+              ),
+
+              //TEXTO CON IMÁGENES
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Colors.orange),
+                  children: [
+                    TextSpan(text: "Texto con imagen: "),
+                    WidgetSpan(
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ),
+                    TextSpan(
+                      text: " Más texto",
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
